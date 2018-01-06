5e45575bee3b7cb05917109a76ec4506ac7fe102
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2018-01-06 09:27:36 +0300

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './styles/fonts/fonts.scss';
